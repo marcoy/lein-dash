44b27a7fda56502c672109694a34a2691c4b833c
Marco Yuen <marcoy@gmail.com>
2015-03-19 23:37:52 -0400

    Experimenting with parsing.

--- src/leiningen/docset/generator.clj
+++ src/leiningen/docset/generator.clj
@@ -2,5 +2,31 @@
   (:require [net.cgrand.enlive-html :as enlive])
   (:import [java.io File]))
 
+(defn selectf [nodes selector]
+  (first (enlive/select nodes selector)))
+
+(defn namespace-info [node]
+  (if-some [name-node (selectf node [[:h2 :#top]])]
+    {:name (enlive/text name-node)
+     :type "namespace"
+     :path ""}))
+
+(defn fn-info [node]
+  (if (and (not (selectf node [(enlive/has [:h4])]))
+           (selectf node [:div.usage :> :code]))
+    {:name (enlive/text (selectf node [:h3]))
+     :type "function"
+     :path ""}))
+
+(defn var-info [node])
+
+(defn protocol-info [node])
+
+(defn macro-info [node])
+
 (defn parse-file [^File html-file]
-  (enlive/html-resource html-file))
+  (let [nodes (enlive/html-resource html-file)]
+    (-> (enlive/select nodes [[:div :#content] :> [:h2 :#top]])
+        first
+        enlive/text)
+    (map fn-info (enlive/select nodes [[:div :#content] :> :.anchor]))))

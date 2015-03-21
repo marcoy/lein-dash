9406a13aa5c5f0df4a8d9a0140e819783577db38
Marco Yuen <marcoy@gmail.com>
2015-03-20 23:21:58 -0400

    Fill :path

--- src/leiningen/docset/codox/parser.clj
+++ src/leiningen/docset/codox/parser.clj
@@ -3,18 +3,21 @@
 
 (def selectf (comp first enlive/select))
 
+(defn id-attr [node]
+  (first (enlive/attr-values node :id)))
+
 (defn namespace-info [node]
   (if-some [name-node (selectf node [[:h2 :#top]])]
     {:name (enlive/text name-node)
      :type "namespace"
-     :path ""}))
+     :path (str "#" (id-attr node))}))
 
 (defn fn-info [node]
   (if (and (not (selectf node [(enlive/has [:h4])]))
            (selectf node [:div.usage :> :code]))
     {:name (enlive/text (selectf node [:h3]))
      :type "function"
-     :path ""}))
+     :path (str "#" (id-attr node))}))
 
 (defn var-info [node]
   (if (and (not (selectf node [:div.usage :> :code]))
@@ -22,18 +25,18 @@
            (not (selectf node [[:h2 :#top]])))
     {:name (enlive/text (selectf node [:h3]))
      :type "variable"
-     :path ""}))
+     :path (str "#" (id-attr node))}))
 
 (defn protocol-info [node]
   (if-some [type-node (selectf node [[:h4.type]])]
     (if (= (enlive/text type-node) "protocol")
       {:name (enlive/text (selectf node [:h3]))
        :type "protocol"
-       :path ""})))
+       :path (str "#" (id-attr node))})))
 
 (defn macro-info [node]
   (if-some [type-node (selectf node [[:h4.type]])]
     (if (= (enlive/text type-node) "macro")
       {:name (enlive/text (selectf node [:h3]))
        :type "macro"
-       :path ""})))
+       :path (str "#" (id-attr node))})))
--- src/leiningen/docset/generator.clj
+++ src/leiningen/docset/generator.clj
@@ -13,4 +13,4 @@
 (defn parse-file [^File html-file]
   (let [nodes (enlive/html-resource html-file)]
     (map #(some-info % [namespace-info fn-info var-info protocol-info macro-info])
-         (enlive/select nodes [[:div :#content] :> :.anchor]))))
+         (enlive/select nodes [[:div :#content] :.anchor]))))
